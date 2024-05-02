9047e1c239d2236b4c4b01fb770f26b8c6d562d1
Hikmatullah Samady <hikmat.samady1@yahoo.com>
2024-05-02 06:45:05 -0400

    feat: add get /api/:date and /api/ endpoints

--- index.js
+++ index.js
@@ -2,31 +2,47 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+  const { date } = req.params;
+  let inputDate = null;
+  if (!isNaN(date)) {
+    inputDate = new Date(parseInt(date));
+  } else {
+    inputDate = new Date(date);
+  }
+  if (isNaN(inputDate.getTime())) {
+    res.json({ error: "Invalid Date" });
+  } else {
+    const unix = inputDate.getTime();
+    const utc = inputDate.toUTCString();
+    res.json({ unix, utc });
+  }
 });
 
-
+app.get("/api/", function (req, res) {
+  const date = new Date();
+  const unix = date.getTime();
+  const utc = date.toUTCString();
+  res.json({ unix, utc });
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
